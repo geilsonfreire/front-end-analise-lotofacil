8286d715999abfb9f4dc55dfc612d1faed0b5192
geilsonfreire <geilosnfreireleite@gmail.com>
2025-01-17 22:04:42 -0300

    refatora a página de Análise Ímpar/Par para utilizar serviços de API, substitui a função fetchSorteios por chamadas diretas à API e implementa controle de estado para evitar análises duplicadas.

--- src/pages/analiseImparPar.jsx
+++ src/pages/analiseImparPar.jsx
@@ -2,36 +2,38 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
-
 // Imports Css
 import "../style/analiseImpaPar.css";
 
 // Imports de Icones
 
-
-
-// Importando o componente
-import { fetchSorteios } from "../utils/fetchSorteios";
+// Importando o serviço de API e a função de processamento
+import apiServices from "../services/apiServices";
 import { processResults } from "../utils/analiyzeOddEven";  
-
-
 
 const AnaliseImpaPar = () => {
     // Estado para armazenar os resultados
     const [analysis, setAnalysis] = useState(null);
-
+    const [isAnalyzed, setIsAnalyzed] = useState(false); // Novo estado para controlar se a análise já foi concluída
 
     useEffect(() => {
         const fetchAndAnalyze = async () => {
-            await fetchSorteios(async (allResults) => {
+            try {
+                const allResults = await apiServices.getAllResults();
                 const analysisResult = await processResults(allResults);
                 setAnalysis(analysisResult);
-                toast.success("Análise concluída com sucesso!");
-            });
+                if (!isAnalyzed) {
+                    toast.success("Análise concluída com sucesso!");
+                    setIsAnalyzed(true); // Atualiza o estado para indicar que a análise foi concluída
+                }
+            } catch (error) {
+                console.error("Erro ao buscar os sorteios:", error.message);
+                toast.error("Erro ao buscar os sorteios.");
+            }
         };
 
         fetchAndAnalyze();
-    }, []);
+    }, [isAnalyzed]); // Adiciona isAnalyzed como dependência para garantir que a função seja chamada apenas uma vez
 
     return (
         <main className="Container-Geral">
@@ -80,4 +82,4 @@ const AnaliseImpaPar = () => {
     );
 };
 
-export default AnaliseImpaPar
+export default AnaliseImpaPar;
